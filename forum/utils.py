975b5719b6e6360799b0297bc6140793319e1fdb
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-04-03 11:12:19 +0100

    updates basemode id

--- forum/utils.py
+++ forum/utils.py
@@ -9,7 +9,7 @@ import os
 class BaseModel(models.Model):
 
     id = models.UUIDField(
-        primary_key=True, db_index=True, default=uuid.uuid4(), editable=False
+        primary_key=True, db_index=True, default=uuid.uuid4, editable=False
     )
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
